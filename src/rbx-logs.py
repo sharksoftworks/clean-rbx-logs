29b5d9bd7f92571adc1fa9a328d372f07d4aeb4e
sharkdev2 <166950471+sharkdev2@users.noreply.github.com>
2024-04-13 21:49:09 +0200

    Add files via upload

---
+++ src/rbx-logs.py
@@ -0,0 +1,25 @@
+import os
+import time
+import pyperclip
+
+from pystyle import Colors, Colorate
+
+print(Colorate.Horizontal(Colors.blue_to_cyan, "Rbx-Logs Remover", True))
+print(Colorate.Horizontal(Colors.blue_to_cyan, "Discord link set to clipboard.", True))
+pyperclip.copy("https://discord.gg/426GMS6SMv")
+time.sleep(1.5)
+os.system("cls")
+
+user = os.getlogin()
+storage = f"C:/Users/{user}/Appdata/Local/Roblox/logs"
+file = os.listdir(storage)
+
+print(Colorate.Horizontal(Colors.blue_to_cyan, "[shark-rbx] Getting log files...", True))
+print(Colorate.Horizontal(Colors.blue_to_cyan, "[shark-rbx] Removing log files...", True))
+for files in file:
+    try:
+        os.remove(file + "/" + files)
+    except Exception as e:
+        var = e
+print(Colorate.Horizontal(Colors.blue_to_cyan, "[shark-rbx] Removed log files...", True))
+input(Colorate.Horizontal(Colors.blue_to_cyan, "[shark-rbx] Press any key to exit...", True))
